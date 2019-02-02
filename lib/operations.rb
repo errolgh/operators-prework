af50fd5281702535bc30c904d34fa6736d0e9d7f
Errol W <ewatsonrei@gmail.com>
2019-02-02 23:27:55 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -17,8 +17,8 @@ end
 def not_safe?(speed)
 	if speed > 60 || speed < 40 ? true : false
 	  return true
-	# elsif speed > 40 && speed < 60
-	  # return false
+	 elsif speed > 40 && speed < 60
+	   return false
   end
 end
 
